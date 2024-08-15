ecdacaa3f5a6d426399b8c40288bdb19766eb5fa
Maidi <mhumaidi437@gmail.com>
2024-08-15 09:17:18 +0800

    Perbaiki file koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -4,11 +4,11 @@ $HOST = 'localhost';
 $DB = 'crud_php_native';
 $USER = 'crud-php-native';
 $PASS = 'cNe4Sia]ov645fVG';
-$conn = mysqli_connect($HOST, $USER, $PASS, $DB);
+$conn = mysqli_connect($HOST, $USER, $PASS, $DB) or die();
 
-if($conn){
-    echo("Berhasil tersambung ke database");
-} else {
+// if($conn){
+//     // echo("Berhasil tersambung ke database");
+// } else {
     
-    die();
-}
+//     die();
+// }
